fc97b94423800ba37972434594c35a8814b83077
Lloyd Eddy <lloyd@Lloyds-MacBook-Pro.local>
2021-12-21 15:36:04 -0500

    WIP creates frist schema

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const Player = require('../models/player')
 
 
 router.get('/', (req, res) => {
---
+++ models/player.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const playerSchema = new Schema ({
+    name: String,
+    avatar: {
+        type: String,
+        default: "https://www.kenyons.com/wp-content/uploads/2017/04/default-image-620x600.jpg"
+    }
+})
+
+module.exports = mongoose.model('Player', playerSchema)
